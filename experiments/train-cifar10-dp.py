cfdf1382e74183850e5e83f7be65c4351f5f5787
Rehn Aki <aki.rehn@helsinki.fi>
2023-06-02 12:50:01 +0300

    First CIFAR-10+ResNet-18+DP version that might work.

--- experiments/train-cifar10-dp.py
+++ experiments/train-cifar10-dp.py
@@ -2,6 +2,8 @@
 import os
 from pathlib import Path
 
+from typing import Callable, Iterable, Protocol
+
 import torch
 import torchmetrics
 
@@ -16,11 +18,7 @@ import timm
 # use Huggingface datasets
 import datasets
 
-# download Huggingface datasets to custom directory if requested
-#if DATA_DIR := os.environ.get('HUGGINGFACE_DATA_DIR'):
-#    datasets.config.DOWNLOADED_DATASETS_PATH = Path(DATA_DIR)
-#    datasets.config.DEFAULT_HF_DATASETS_CACHE = = Path(DATA_DIR)
-# NB: These can be set with an environment variable HF_DATASETS_CACHE
+# NB: Set data cache directory with environment variable HF_DATASETS_CACHE
 
 # You are using a CUDA device ('AMD Radeon Graphics') that has Tensor Cores.
 # To properly utilize them, you should set `torch.set_float32_matmul_precision('medium' | 'high')`
@@ -55,28 +53,59 @@ class MyHuggingFaceCIFAR10DataModule(L.LightningDataModule):
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(
-                self.train,
-                batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                num_workers=self.num_workers,
-            )
+            self.train,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=self.num_workers,
+        )
 
     def val_dataloader(self):
         return torch.utils.data.DataLoader(
-                self.val,
-                batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                num_workers=self.num_workers,
+            self.val,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=self.num_workers,
+        )
+
+class MyHuggingFaceCIFAR10DataModuleDP(MyHuggingFaceCIFAR10DataModule):
+    def train_dataloader(self):
+        # check if we should do DDP
+        distributed = isinstance(
+            self.trainer.model,
+            (
+                opacus.distributed.DifferentiallyPrivateDistributedDataParallel,
+                torch.nn.parallel.DistributedDataParallel,
             )
+        )
+
+        # For DP we need dataloader with Poisson uniform sampling
+        # 2b - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
+        dataloader = super().train_dataloader()
+        return opacus.data_loader.DPDataLoader.from_data_loader(dataloader, distributed=distributed)
 
 class CIFAR10ClassificationModel(L.LightningModule):
-    def __init__(self, num_classes: int = 10):
+    def __init__(
+        self,
+        optimizer: Callable[[Iterable], torch.optim.Optimizer] = torch.optim.Adam,
+        lr: float = 1e-3,
+        num_classes: int = 10,
+    ):
         super().__init__()
+        self.optimizer = optimizer
+        self.lr = lr
 
         self.model = timm.create_model('resnet18', num_classes=num_classes)
-
         self.criterion = torch.nn.CrossEntropyLoss()
         self.accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=num_classes)
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer(self.parameters())
+
+        # HACK(ish): jsonargparse does not seem to flexible enough to support learning rate in init!
+        for g in optimizer.param_groups:
+            g['lr'] = self.lr
+
+        return optimizer
 
     def forward(self, x):
         return self.model(x)
@@ -104,82 +133,87 @@ class CIFAR10ClassificationModel(L.LightningModule):
         self.log('valid_loss', loss, on_epoch=True, on_step=False, prog_bar=True)
 
 class CIFAR10ClassificationModelDP(CIFAR10ClassificationModel):
-    def __init__(self):
+    def __init__(
+        self,
+        optimizer: Callable[[Iterable], torch.optim.Optimizer] = torch.optim.Adam,
+        lr: float = 1e-3,
+        num_classes: int = 10,
+        noise_multiplier: float = 1.0,
+        max_grad_norm: float = 1.0,
+        delta: float = None, # defaults to training dataset size
+    ):
         super().__init__()
 
-        # resnet18 has batchnorm layers that we can't use with DP-SGD
-        dp_model = opacus.validators.ModuleValidator.fix(self.model)
+        self.noise_multiplier = noise_multiplier
+        self.max_grad_norm = max_grad_norm
+        self.delta = delta
+
+        self.privacy_engine = opacus.PrivacyEngine()
+
+        # some layers are not supported for DP
+        # automatically fix unsupported layers (e.g. BatchNorm -> GroupNorm)
+        if not opacus.validators.ModuleValidator.is_valid(self.model):
+            self.model = opacus.validators.ModuleValidator.fix(self.model)
 
         # For DP we need per-sample gradients that Opacus can handle for us.
         # 2a - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
-        dp_model = opacus.GradSampleModule(dp_model)
-        #import ipdb
-        #ipdb.set_trace()
-        #dp_model.forbid_grad_accumulation()
-        #dp_model.register_forward_pre_hook(opacus.privacy_engine.forbid_accumulation_hook)
-
+        dp_model = opacus.GradSampleModule(self.model)
         self.model = dp_model
 
-class MyHuggingFaceCIFAR10DataModuleDP(MyHuggingFaceCIFAR10DataModule):
-    def train_dataloader(self):
-        # For DP we need dataloader with Poisson uniform sampling
-        # 2b - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
-        # XXX: Distributed dataloader
-        dataloader = super().train_dataloader()
-        return opacus.data_loader.DPDataLoader.from_data_loader(dataloader)
+    def configure_optimizers(self):
+        optimizer = super().configure_optimizers()
 
-class SetDPOptimizerCallback(L.pytorch.callbacks.Callback):
-    def __init__(self, noise_multiplier: float = 1.0, max_grad_norm: float = 1.0):
-        super().__init__()
-        self.noise_multiplier = noise_multiplier
-        self.max_grad_norm = max_grad_norm
-
-    def on_train_start(self, trainer, pl_module):
-        if len(trainer.optimizers) > 1:
-            raise NotImplementedError('Support for more than one optimizer not implemented.')
-
-        optimizer = trainer.optimizers[0]
+        dataloader = self.trainer.datamodule.train_dataloader()
 
         # For DP we need optimizer that clips the per-sample gradients and adds noise to the gradient.
         # 2c - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
-        sample_rate = trainer.train_dataloader.sample_rate
-        expected_batch_size = int(len(trainer.train_dataloader.dataset) * sample_rate)
-
-        dp_optimizer = opacus.optimizers.DPOptimizer(
-            optimizer=optimizer,
-            noise_multiplier=self.noise_multiplier,
-            max_grad_norm=self.max_grad_norm,
-            expected_batch_size=expected_batch_size,
+        # 3d - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
+        _, dp_optimizer, _ = self.privacy_engine.make_private(
+            module = self,
+            optimizer = optimizer,
+            data_loader = dataloader,
+            noise_multiplier = self.noise_multiplier,
+            max_grad_norm = self.max_grad_norm,
         )
 
-        # Setup privacy accountant.
-        # 3d - https://github.com/pytorch/opacus/blob/main/tutorials/intro_to_advanced_features.ipynb
-        accountant = opacus.accountants.RDPAccountant()
-        dp_optimizer.attach_step_hook(accountant.get_optimizer_hook_fn(sample_rate=sample_rate))
+        return dp_optimizer
 
-        trainer.optimizers = [dp_optimizer]
+    def training_step(self, batch, batch_idx):
+        loss = super().training_step(batch, batch_idx)
 
-def cli_main():
+        print(f'     Model class: {self.trainer.model.__class__}')
+        print(f' Optimizer class: {self.trainer.optimizers[0].__class__}')
+        print(f'Dataloader class: {self.trainer.train_dataloader.__class__}')
 
-    callbacks = [
-        SetDPOptimizerCallback(noise_multiplier=1.0, max_grad_norm=1.0),
-    ]
+        epsilon = self._get_epsilon()
+        self.log('epsilon', epsilon, on_epoch=True, on_step=False, prog_bar=True)
 
+        return loss
+
+    def _get_epsilon(self):
+        if not self.delta:
+            N = len(self.trainer.train_dataloader.dataset)
+            delta = 1/N
+        else:
+            delta = self.delta
+
+        epsilon = self.privacy_engine.get_epsilon(delta)
+
+        return epsilon
+
+def main():
     cli = LightningCLI(
         CIFAR10ClassificationModelDP,
         MyHuggingFaceCIFAR10DataModuleDP,
         trainer_defaults = {
             'max_epochs': 10,
-            'callbacks': callbacks,
         },
         save_config_kwargs = {
             'overwrite': True
         },
+        auto_configure_optimizers = False,
     )
     cli.trainer.fit(cli.model, datamodule=cli.datamodule)
 
 if __name__ == '__main__':
-    if os.environ.get('LIGHTNING_VANILLA') == 'true':
-        main()
-    else:
-        cli_main()
+    main()
