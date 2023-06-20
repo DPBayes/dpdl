fe1bdf79757f8392b79236affd26532f77b5333f
Aki Rehn <aki.rehn@helsinki.fi>
2023-06-20 13:50:22 +0300

    Report losses in reasonable precision.

--- experiments/train-cifar10-dp-fabric.py
+++ experiments/train-cifar10-dp-fabric.py
@@ -330,13 +330,13 @@ class PrintStateCallback():
         print(f' - Starting epoch {epoch+1}.')
 
     def on_train_epoch_end(self, trainer, epoch, loss):
-        print(f' - Epoch finished, loss: {loss}.')
+        print(f' - Epoch finished, loss: {loss:.3f}.')
 
     #def on_train_batch_start(self, trainer, batch_idx, batch):
     #    print(f'  - Start processing batch {batch_idx+1}.')
 
     def on_train_batch_end(self, trainer, batch_idx, batch, loss):
-        print(f'  - Processed batch {batch_idx+1}, loss: {loss:.2f}')
+        print(f'  - Processed batch {batch_idx+1}, loss: {loss:.3f}')
 
 def main():
     # training params
