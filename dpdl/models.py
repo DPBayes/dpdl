1198650dc8ab89b720705b1a5271ac2288da6360
Aki Rehn <aki.rehn@helsinki.fi>
2023-06-27 12:48:48 +0300

    Add pretrained as model arguments.

--- dpdl/models.py
+++ dpdl/models.py
@@ -3,10 +3,18 @@ import torch
 import timm
 
 class ImageClassificationModel(torch.nn.Module):
-    def __init__(self, model_name: str ='resnet18', num_classes: int = 10):
+    def __init__(self,
+            model_name: str ='resnet18',
+            num_classes: int = 10,
+            pretrained: bool = True,
+        ):
         super().__init__()
         self.num_classes = num_classes
-        self.model = timm.create_model(model_name, num_classes=num_classes)
+        self.model = timm.create_model(
+            model_name,
+            num_classes=num_classes,
+            pretrained=pretrained
+        )
 
         if not opacus.validators.ModuleValidator.is_valid(self.model):
             self.model = opacus.validators.ModuleValidator.fix(self.model)
