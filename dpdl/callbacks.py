35623496ca8781b5f4bbcb9e8feb7b76a1b24338
Aki Rehn <aki.rehn@helsinki.fi>
2023-07-10 14:34:14 +0300

    Add bit more debug.

--- dpdl/callbacks.py
+++ dpdl/callbacks.py
@@ -1,7 +1,8 @@
+import torch
+
 class Callback():
     def _is_global_zero(self, trainer):
-        if not trainer.fabric.is_global_zero:
-            return
+        return trainer.fabric.is_global_zero
     def on_train_start(self, trainer):
         pass
     def on_train_end(self, trainer):
@@ -26,25 +27,34 @@ class Callback():
 class PrintStateCallback(Callback):
     def on_train_start(self, trainer):
         if self._is_global_zero(trainer):
-            print(f'Starting training for {trainer.epochs} epochs.')
+            print(f'!!! Starting training for {trainer.epochs} epochs.')
 
     def on_train_end(self, trainer):
         if self._is_global_zero(trainer):
-            print('Training finished.')
+            print('!!! Training finished.')
 
     def on_train_epoch_start(self, trainer, epoch):
         if self._is_global_zero(trainer):
-            print(f' - Starting epoch {epoch+1}.')
+            print(f' - Starting training epoch {epoch+1}.')
 
     def on_train_epoch_end(self, trainer, epoch, loss):
         if self._is_global_zero(trainer):
-            print(f' - Epoch finished, loss: {loss:.3f}.')
+            print(f' - Epoch {epoch+1} finished. Loss: {loss:.4f}.')
 
-    #def on_train_batch_start(self, trainer, batch_idx, batch):
-    #    if self._is_global_zero(trainer):
-    #       print(f'  - Start processing batch {batch_idx+1}.')
+#    def on_train_batch_end(self, trainer, batch_idx, batch, loss):
+#        if self._is_global_zero(trainer):
+#            print(f'  - Processed batch {batch_idx+1}, loss: {loss:.4f}')
 
-    def on_train_batch_end(self, trainer, batch_idx, batch, loss):
+#    def on_validation_epoch_start(self, trainer, epoch):
+#        if self._is_global_zero(trainer):
+#            if epoch:
+#                print(f' - Starting validation epoch {epoch+1}.')
+#            else:
+#                print(f' - Starting validation.')
+
+    def on_validation_epoch_end(self, trainer, epoch, loss):
         if self._is_global_zero(trainer):
-            print(f'  - Processed batch {batch_idx+1}, loss: {loss:.3f}')
-
+            if epoch:
+                print(f' - Validation epoch {epoch+1} finished. Loss: {loss:.4f}.')
+            else:
+                print(f' - Validation finished. Loss: {loss:.4f}.')
