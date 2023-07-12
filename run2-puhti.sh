45b88b5188d05c98d37d20835ec62bd00d50dcd5
Aki Rehn <aki.rehn@helsinki.fi>
2023-07-12 15:52:03 +0300

    Add Slurm scripts for Puhti.

---
+++ run2-puhti.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=project_2003275
+#SBATCH --partition=gputest
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --gres=gpu:a100:2
+#SBATCH --time=00:15:00
+#SBATCH --mem-per-gpu=32G
+#SBATCH --error=slurm-%x.%j.out
+#SBATCH --output=slurm-%x.%j.stdout
+
+module purge
+module load pytorch
+module list
+
+export PROJECT="project_2003275"
+export DATA_DIR="/scratch/$PROJECT/aki_temp/data"
+export HF_DATASETS_CACHE="$DATA_DIR/huggingface"
+#export SING_IMAGE="/scratch/project_2003275/aki_temp/singularity-images/pytorch_2.0.1_csc.sif"
+#export CUDA_LAUNCH_BLOCKING=1
+export _TYPER_STANDARD_TRACEBACK=1
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+
+source /scratch/$PROJECT/aki_temp/venvs/dpdl/bin/activate
+
+echo "USING MASTER PORT: $MASTER_PORT" >&2
+set -xv
+srun python3 -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:$MASTER_PORT $*
