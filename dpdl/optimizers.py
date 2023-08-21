b8cbf1fd130c573bd61b8d1ebdee0db13db0901f
Rehn Aki <aki.rehn@helsinki.fi>
2023-08-21 10:21:56 +0300

    Very minor syntax changes.
    - Remove parens around raise calls.
    - Remove parens from class definitions that don't inherit anything.

--- dpdl/optimizers.py
+++ dpdl/optimizers.py
@@ -1,6 +1,6 @@
 import torch
 
-class OptimizerFactory():
+class OptimizerFactory:
     @staticmethod
     def get_optimizer(configuration: dict, hyperparams: dict, model: torch.nn.Module):
         optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams['learning_rate'])
