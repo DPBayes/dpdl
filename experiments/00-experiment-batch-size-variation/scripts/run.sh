07da00503f29f6b21cc1a58ece035de92521cce9
Aki Rehn <aki.rehn@helsinki.fi>
2023-12-22 11:57:19 +0200

    Run script for batch size variation experiment

---
+++ experiments/00-experiment-batch-size-variation/scripts/run.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+##################################################
+### Experiment: 00-experiment-batch-size-variation
+##################################################
+
+# Base configurations
+EXPERIMENT_BASE="00-experiment-batch-size-variation"
+LOG_DIR="/projappl/$PROJECT/dpdl/experiments/$EXPERIMENT_BASE/data"
+mkdir -p $LOG_DIR
+
+# Experiment parameters
+MODELS=("vit_base_patch16_224.augreg_in21k" "resnetv2_50x1_bit.goog_in21k")
+DATASETS=("cifar10" "cifar100")  # Adjust as necessary, e.g., add "cifar10" if needed
+SUBSET_SIZES=("0.1" "1.0")  # Assuming you want 10% and full dataset for CIFAR-100
+PEFT="--peft film"
+
+# Other settings
+SEED=42
+N_TRIALS=20
+PRIVACY="--privacy"
+USE_STEPS="--use-steps"
+NORMALIZE_CLIPPING="--normalize-clipping"
+ZERO_HEAD="--zero-head"
+OPTUNA_JOURNAL="--optuna-journal $LOG_DIR/optuna.journal"
+OPTUNA_RESUME="--no-optuna-resume" # should we resume the study?
+OVERWRITE_EXPERIMENT="--overwrite-experiment" # for easy requeuing
+
+# Loop over configurations
+for model in "${MODELS[@]}"
+do
+    for dataset in "${DATASETS[@]}"
+    do
+        # Adjust number of classes based on the dataset
+        if [ "$dataset" == "cifar100" ]; then
+            NUM_CLASSES=100
+        else
+            NUM_CLASSES=10
+        fi
+
+        for subset_size in "${SUBSET_SIZES[@]}"
+        do
+            if [ "$subset_size" == "0.1" ]; then
+                BATCH_SIZES="256 512 1024 2048 4096 full"
+                EPSILONS="0.25 0.5 1 2 4 8"
+            else
+                BATCH_SIZES="256 512 1024 2048 4096 8192 16384 32768 full"
+                EPSILONS="1"
+            fi
+
+            for epsilon in $EPSILONS
+            do
+                for batch_size in $BATCH_SIZES
+                do
+                    EXPERIMENT_NAME="${model}_${dataset}_Subset${subset_size}_Epsilon${epsilon}_BatchSize${batch_size}"
+                    OPTUNA_CONFIG="conf/optuna_hypers-subset${subset_size}.conf"
+
+                    echo sbatch -J $EXPERIMENT_NAME run8.sh run.py optimize \
+                        --num-workers 7 \
+                        --model-name $model \
+                        --dataset-name $dataset \
+                        --subset-size $subset_size \
+                        --num-classes $NUM_CLASSES \
+                        --batch_size $batch_size \
+                        --target-hypers epochs \
+                        --target-hypers learning_rate \
+                        --target-hypers max_grad_norm \
+                        --target-epsilon $epsilon \
+                        --n-trials $N_TRIALS \
+                        --seed $SEED \
+                        --physical-batch-size 40 \
+                        --optuna-config $OPTUNA_CONFIG \
+                        --optuna-target-metric MulticlassAccuracy \
+                        --optuna-direction maximize \
+                        --experiment-name $EXPERIMENT_NAME \
+                        --log-dir $LOG_DIR \
+                        $PEFT \
+                        $PRIVACY \
+                        $USE_STEPS \
+                        $NORMALIZE_CLIPPING \
+                        $OVERWRITE_EXPERIMENT \
+                        $OPTUNA_RESUME \
+                        $OPTUNA_JOURNAL
+                done
+            done
+        done
+    done
+done
