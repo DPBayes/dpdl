b764d1e2e9fc4cbab16d6109bae11b50e876e583
Aki Rehn <aki.rehn@helsinki.fi>
2024-01-15 10:14:43 +0200

    Add script to resume experiments by name.

---
+++ bin/resume-cancelled-jobs-byname.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to display usage/help
+usage() {
+    echo "Usage: $0 <EXPERIMENT_BASE> <MAX_TRIALS> <EXPERIMENT_NAME>"
+    echo "Example: $0 /path/to/experiments 20"
+}
+
+# Check if the number of arguments is less than 3, display usage
+if [ $# -lt 3 ]; then
+    usage
+    exit 1
+fi
+
+# Get command line arguments to variables
+EXPERIMENT_BASE=$1
+MAX_TRIALS=$2
+EXPERIMENT_NAME=${3:-$LOG_PATTERN}
+
+# Get the number of completed trials using the Python script
+N_TRIALS=$(python bin/get_n_trials.py --optuna-journal "$EXPERIMENT_BASE"/data/optuna.journal --study-name "$EXPERIMENT_NAME")
+
+# Calculate remaining trials
+REMAINING_TRIALS=$((MAX_TRIALS - N_TRIALS))
+
+# Resume the experiment
+if [ "$REMAINING_TRIALS" -gt -1 ]; then
+    bash "$EXPERIMENT_BASE/scripts/resume.sh" "$EXPERIMENT_NAME" "$REMAINING_TRIALS"
+fi
