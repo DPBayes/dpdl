6d69895e3497c1314e8e414ed2b9b1905f8a5a51
Aki Rehn <aki.rehn@helsinki.fi>
2024-02-27 14:22:56 +0200

    Do not use Timm transforms in WRN or KoskelaNet
    - The Wide Resnet and KoskelaNet models for experiment 06 were currently
      using the default timm tranforms which transformed the images to
      imagenet size (224x224). Switched to use custom transformations that
      keep the images in 32x32.

--- dpdl/models/model_factory.py
+++ dpdl/models/model_factory.py
@@ -4,6 +4,7 @@ import timm
 from .model_base import ModelBase
 from .timm_model import TimmModel
 from .wide_resnet import WideResNet
+from .koskela_model import KoskelaNet
 
 from dpdl.configurationmanager import Configuration, Hyperparameters
 from dpdl.peft import PeftFactory
@@ -24,12 +25,18 @@ class ModelFactory:
         - A tuple of (ModelBase instance, Data Transforms).
         """
 
+        transforms = {}  # No default transforms
         model_instance = None
+
         if configuration.model_name.startswith('wrn-'):
             # Parse depth and width from model_name, e.g., 'wrn-16-4'
             parts = configuration.model_name.split('-')
             depth, width = int(parts[1]), int(parts[2])
             model_instance = WideResNet(depth=depth, width=width, num_classes=configuration.num_classes)
+            transforms = model_instance.get_transforms()
+        elif configuration.model_name == 'koskela-net':
+            model_instance = KoskelaNet()
+            transforms = model_instance.get_transforms()
         else:
             # Default to using TimmModel
             model_instance = TimmModel(
@@ -38,12 +45,12 @@ class ModelFactory:
                 pretrained=configuration.pretrained,
             )
 
+            # Resolve data config and create transforms
+            model_config = timm.data.resolve_data_config({}, model=model.model)
+            transforms = timm.data.transforms_factory.create_transform(**model_config)
+
         # Wrap the instantiated model with ModelBase
         model = ModelBase(model_instance=model_instance, num_classes=configuration.num_classes)
-
-        # Resolve data config and create transforms
-        model_config = timm.data.resolve_data_config({}, model=model.model)
-        transforms = timm.data.transforms_factory.create_transform(**model_config)
 
         # zero the head weights?
         if configuration.zero_head:
