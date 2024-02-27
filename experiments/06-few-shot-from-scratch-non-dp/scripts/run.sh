35578c7adce5296e67c284722fadbf224a260117
Aki Rehn <aki.rehn@helsinki.fi>
2024-02-27 14:21:02 +0200

    Change wrn-40-4 -> koskela-net model in run script

--- experiments/06-few-shot-from-scratch-non-dp/scripts/run.sh
+++ experiments/06-few-shot-from-scratch-non-dp/scripts/run.sh
@@ -15,7 +15,7 @@ JOB_STATUS_LOG="$LOG_DIR/submitted_jobs.log"
 touch $JOB_STATUS_LOG  # Create the file if it doesn't exist
 
 # Experiment parameters
-MODELS=("wrn-16-4" "wrn-40-4")
+MODELS=("wrn-16-4" "koskela-net")
 ALL_SHOTS=(1 5 10 25 50 100 250 500 1000 1500 2000)
 DATASET="cifar10"
 NUM_CLASSES=10
