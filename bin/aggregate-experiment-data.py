11c50656d76c5200c23bf4d646f913f720b1f9fd
Aki Rehn <aki.rehn@helsinki.fi>
2024-02-29 13:23:06 +0200

    Aggregation script to skip unfinished experiments
    - For easier use, changed experiment data aggregation script to print a
      warning and skip unfinished experiments.

--- bin/aggregate-experiment-data.py
+++ bin/aggregate-experiment-data.py
@@ -27,6 +27,11 @@ def process_experiment_directory(directory, pattern=None):
             continue
 
         experiment_path = os.path.join(directory, entry)
+
+        if not os.path.exists(os.path.join(experiment_path, 'runtime')):
+            print(f'Warning: Experiment {entry} not finished. Skipping.')
+            continue
+
         if os.path.isdir(experiment_path):
             try:
                 experiment_data = {
