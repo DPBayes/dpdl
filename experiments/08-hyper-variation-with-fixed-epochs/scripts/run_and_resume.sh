0c303246c04d49c042f3ab1aa41fcfab53b7b3b7
Aki Rehn <aki.rehn@helsinki.fi>
2024-03-15 15:08:04 +0200

    Finish experiment 08 run script.

--- experiments/08-hyper-variation-with-fixed-epochs/scripts/run_and_resume.sh
+++ experiments/08-hyper-variation-with-fixed-epochs/scripts/run_and_resume.sh
@@ -5,12 +5,16 @@
 
 set -euo pipefail
 
+if ! python -c "import optuna" &> /dev/null; then
+    echo "Error: 'optuna' module not found. Please make sure you have correct environment activated."
+    exit 1
+fi
+
 EXPERIMENT_BASES=("08-hyper-variation-with-fixed-epochs__batch_size_variation"
                   "08-hyper-variation-with-fixed-epochs__max_grad_norm_variation"
                   "08-hyper-variation-with-fixed-epochs__learning_rate_variation")
 
 # Base configurations
-PROJECT="08-hyper-variation-with-fixed-epochs"
 EPOCH_VALUES=("10" "40")
 MODELS=("vit_base_patch16_224.augreg_in21k" "resnetv2_50x1_bit.goog_in21k")
 DATASETS=("cifar10" "cifar100")
@@ -25,20 +29,61 @@ SEED=42
 DEFAULT_N_TRIALS=20
 OTHER_SETTINGS="--privacy --use-steps --normalize-clipping --zero-head --peft film"
 
+# Function to check if a job has been submitted
+function is_job_submitted() {
+    grep -Fxq "$1" $JOB_STATUS_LOG
+}
+
+# Function to check if a job is already in queue
+function is_job_in_queue() {
+    local experiment_name=$1
+    if squeue --me -o "%.150j" | grep -q "$experiment_name"; then
+        return 0  # Job is in queue
+    else
+        return 1  # Job is not in queue
+    fi
+}
+
 function submit_experiment() {
     local experiment_base=$1
     local model=$2
-    local dataset=$3
-    local subset_size=$4
-    local epoch=$5
-    local hyper_value=$6  # Batch size, Max Grad Norm, or Learning Rate depending on the experiment
-    local hyper_name=$7   # "batch_size", "max_grad_norm", or "learning_rate"
+    local epsilon=$3
+    local dataset=$4
+    local subset_size=$5
+    local epoch=$6
+    local hyper_value=$7
+    local hyper_name=$8
+    local hyper_name_experiment=$(echo $hyper_name | sed 's/_\([a-z]\)/\U\1/g;s/^./\U&/')
 
-    local LOG_DIR="/projappl/${PROJECT}/dpdl/experiments/${experiment_base}/data"
+    local LOG_DIR="/projappl/$PROJECT/dpdl/experiments/${experiment_base}/data"
     mkdir -p "${LOG_DIR}"
 
-    local EXPERIMENT_NAME="${model}_${dataset}_Subset${subset_size}_Epoch${epoch}_${hyper_name^}${hyper_value}"
-    local CMD_PREFIX="echo sbatch -J ${EXPERIMENT_NAME} run8.sh run.py optimize --num-workers 7 --model-name ${model} --dataset-name ${dataset} --subset-size ${subset_size} --num-classes ${NUM_CLASSES} --epochs ${epoch} --target-epsilon 1 --n-trials ${DEFAULT_N_TRIALS} --seed ${SEED} --physical-batch-size 40 --optuna-config conf/optuna_hypers-subset${subset_size}.conf --optuna-target-metric MulticlassAccuracy --optuna-direction maximize --experiment-name ${EXPERIMENT_NAME} --log-dir ${LOG_DIR} ${OTHER_SETTINGS} --${hyper_name} ${hyper_value}"
+    local JOB_STATUS_LOG="$LOG_DIR/submitted_jobs.log"
+    touch $JOB_STATUS_LOG  # Create the file if it doesn't exist
+
+    local EXPERIMENT_NAME="${model}_${dataset}_Subset${subset_size}_Epsilon${epsilon}_Epoch${epoch}_${hyper_name_experiment}${hyper_value}"
+
+    if is_job_in_queue $EXPERIMENT_NAME; then
+        echo "Experiment $EXPERIMENT_NAME is already in the queue."
+        continue  # Skip to the next iteration
+    fi
+
+
+    # Determine target hyperparameters based on the fixed hyperparameter
+    local TARGET_HYPERS=""
+    case $hyper_name in
+        "batch_size")
+            TARGET_HYPERS="--target-hypers max_grad_norm --target-hypers learning_rate"
+            ;;
+        "max_grad_norm")
+            TARGET_HYPERS="--target-hypers batch_size --target-hypers learning_rate"
+            ;;
+        "learning_rate")
+            TARGET_HYPERS="--target-hypers batch_size --target-hypers max_grad_norm"
+            ;;
+    esac
+
+    local CMD_PREFIX="echo sbatch -J ${EXPERIMENT_NAME} run8.sh run.py optimize --num-workers 7 --model-name ${model} --dataset-name ${dataset} --subset-size ${subset_size} --num-classes ${NUM_CLASSES} --epochs ${epoch} $TARGET_HYPERS --target-epsilon $epsilon --n-trials ${DEFAULT_N_TRIALS} --seed ${SEED} --physical-batch-size 40 --optuna-config conf/optuna_hypers-subset${subset_size}.conf --optuna-target-metric MulticlassAccuracy --optuna-direction maximize --experiment-name ${EXPERIMENT_NAME} --log-dir ${LOG_DIR} ${OTHER_SETTINGS} --${hyper_name} ${hyper_value}"
 
     # Check for and handle resuming of experiments
     local EXPERIMENT_DIR="${LOG_DIR}/${EXPERIMENT_NAME}"
@@ -53,57 +98,91 @@ function submit_experiment() {
         touch "${LOG_DIR}/optuna.journal"
     fi
 
-    local N_TRIALS=$(python bin/get_n_trials.py --optuna-journal "${LOG_DIR}/optuna.journal" --study-name "${EXPERIMENT_NAME}")
 
-    if [ "$N_TRIALS" -eq -1 ]; then
+    if is_job_submitted $EXPERIMENT_NAME; then
+        local N_TRIALS=$(python bin/get_n_trials.py --optuna-journal "${LOG_DIR}/optuna.journal" --study-name "${EXPERIMENT_NAME}")
+
+        if [ "$N_TRIALS" -eq -1 ]; then
+            local REMAINING_TRIALS=$DEFAULT_N_TRIALS
+            local OPTUNA_RESUME="--no-optuna-resume"
+            local OVERWRITE_EXPERIMENT="--overwrite-experiment"
+        else
+            local REMAINING_TRIALS=$((DEFAULT_N_TRIALS - N_TRIALS))
+            local OPTUNA_RESUME="--optuna-resume"
+            local OVERWRITE_EXPERIMENT="--no-overwrite-experiment"
+        fi
+
+        if [ "$REMAINING_TRIALS" -le 0 ]; then
+            echo "Experiment ${EXPERIMENT_NAME} has already completed the designated number of trials."
+            return
+        fi
+    else
         local REMAINING_TRIALS=$DEFAULT_N_TRIALS
         local OPTUNA_RESUME="--no-optuna-resume"
-    else
-        local REMAINING_TRIALS=$((DEFAULT_N_TRIALS - N_TRIALS))
-        local OPTUNA_RESUME="--optuna-resume"
-    fi
-
-    if [ "$REMAINING_TRIALS" -le 0 ]; then
-        echo "Experiment ${EXPERIMENT_NAME} has already completed the designated number of trials."
-        return
+        local OVERWRITE_EXPERIMENT="--overwrite-experiment"
+        local N_TRIALS=-1  # Signal logging that this is a new experiment.
     fi
 
     # Adjust CMD_PREFIX based on remaining trials and whether to resume
-    CMD_PREFIX+=" --n-trials ${REMAINING_TRIALS} ${OPTUNA_RESUME}"
+    CMD_PREFIX+=" --n-trials ${REMAINING_TRIALS} ${OPTUNA_RESUME} $OVERWRITE_EXPERIMENT"
 
     echo "Submitting: ${EXPERIMENT_NAME} with ${REMAINING_TRIALS} trials"
     eval $CMD_PREFIX
+    local SBATCH_EXIT_CODE=$?
+
+    if [ $SBATCH_EXIT_CODE -eq 0 ]; then
+        echo "Job $EXPERIMENT_NAME submitted successfully."
+        if [ "$N_TRIALS" -eq -1 ]; then
+            echo $EXPERIMENT_NAME >> $JOB_STATUS_LOG  # Log the submission for new experiments only
+        fi
+    else
+        echo "Submission of $EXPERIMENT_NAME failed with exit code $SBATCH_EXIT_CODE."
+        exit 1
+    fi
 }
 
 # Main loop
-for experiment_base in "${EXPERIMENT_BASES[@]}"; do
-    for model in "${MODELS[@]}"; do
-        for dataset in "${DATASETS[@]}"; do
+for experiment_base in "${EXPERIMENT_BASES[@]}"
+do
+    for model in "${MODELS[@]}"
+    do
+        for dataset in "${DATASETS[@]}"
+        do
             NUM_CLASSES=10
             [ "$dataset" == "cifar100" ] && NUM_CLASSES=100
 
-            for subset_size in "${SUBSET_SIZES[@]}"; do
+            for subset_size in "${SUBSET_SIZES[@]}"
+            do
+
+                EPSILONS="0.25 0.5 1 2 4 8"
+                [ "$subset_size" == "1.0" ] && EPSILONS="1"
+
                 BATCH_SIZES=("${BATCH_SIZES_SUBSET_01[@]}")
                 [ "$subset_size" == "1.0" ] && BATCH_SIZES=("${BATCH_SIZES_SUBSET_1[@]}")
 
-                for epoch in "${EPOCH_VALUES[@]}"; do
-                    case $experiment_base in
-                        *"batch_size_variation"*)
-                            for batch_size in "${BATCH_SIZES[@]}"; do
-                                submit_experiment $experiment_base $model $dataset $subset_size $epoch $batch_size "batch_size"
-                            done
-                            ;;
-                        *"max_grad_norm_variation"*)
-                            for max_grad_norm in "${MAX_GRAD_NORMS[@]}"; do
-                                submit_experiment $experiment_base $model $dataset $subset_size $epoch $max_grad_norm "max_grad_norm"
-                            done
-                            ;;
-                        *"learning_rate_variation"*)
-                            for learning_rate in "${LEARNING_RATES[@]}"; do
-                                submit_experiment $experiment_base $model $dataset $subset_size $epoch $learning_rate "learning_rate"
-                            done
-                            ;;
-                    esac
+                for epoch in "${EPOCH_VALUES[@]}"
+                do
+                    for epsilon in $EPSILONS
+                    do
+
+                        case $experiment_base in
+                            *"batch_size_variation"*)
+                                for batch_size in "${BATCH_SIZES[@]}"; do
+                                    submit_experiment $experiment_base $model $epsilon $dataset $subset_size $epoch $batch_size "batch_size"
+                                done
+                                ;;
+                            *"max_grad_norm_variation"*)
+                                for max_grad_norm in "${MAX_GRAD_NORMS[@]}"; do
+                                    submit_experiment $experiment_base $model $epsilon $dataset $subset_size $epoch $max_grad_norm "max_grad_norm"
+                                done
+                                ;;
+                            *"learning_rate_variation"*)
+                                for learning_rate in "${LEARNING_RATES[@]}"; do
+                                    submit_experiment $experiment_base $model $epsilon $dataset $subset_size $epoch $learning_rate "learning_rate"
+                                done
+                                ;;
+                        esac
+                    done
                 done
             done
         done
