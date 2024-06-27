f965dad54b78bf11f4f0223771a9978f059d8c1b
Aki Rehn <aki.rehn@helsinki.fi>
2024-06-27 15:10:14 +0300

    Multi-class accuracy with micro averaging metric
    - When working with imbalanced datasets, we might want to use other
      metrics than just multi-class accuracy with macro averaging.
      Implemented addition of multi-class accuracy with micro averaging with
      key 'MulticlassAccuracyWithMicro'

--- dpdl/models/model_base.py
+++ dpdl/models/model_base.py
@@ -18,21 +18,32 @@ class ModelBase(torch.nn.Module):
         self._criterion = torch.nn.CrossEntropyLoss().cuda()
 
         # let's track the training accuracy
-        self.train_metrics = torchmetrics.MetricCollection([
-            torchmetrics.classification.MulticlassAccuracy(
+        self.train_metrics = torchmetrics.MetricCollection({
+            'MulticlassAccuracy': torchmetrics.classification.MulticlassAccuracy(
                 num_classes=self.num_classes,
+                average='macro',
             ).cuda(),
-        ])
+            'MulticlassAccuracyWithMicro': torchmetrics.classification.MulticlassAccuracy(
+                num_classes=self.num_classes,
+                average='micro',
+            ).cuda(),
+        })
 
         # we only validate on rank 0, so there's no need to
         # synchronize when calculating the metrics. although,
         # the flag is probably redundant.
-        self.valid_metrics = torchmetrics.MetricCollection([
-            torchmetrics.classification.MulticlassAccuracy(
+        self.valid_metrics = torchmetrics.MetricCollection({
+            'MulticlassAccuracy': torchmetrics.classification.MulticlassAccuracy(
                 num_classes=self.num_classes,
+                average='macro',
                 sync_on_compute=False,
             ).cuda(),
-        ])
+            'MulticlassAccuracyWithMicro': torchmetrics.classification.MulticlassAccuracy(
+                num_classes=self.num_classes,
+                average='micro',
+                sync_on_compute=False,
+            ).cuda(),
+        })
 
     def forward(self, x):
         if self.use_feature_cache:
