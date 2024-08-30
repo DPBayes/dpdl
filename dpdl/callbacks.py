da80c96fc6956721b71121cdfe9ebf5b5b5dee70
Linzh <linzh7@yahoo.com>
2024-08-30 09:39:30 +0300

    Add support for clip proportion and acc recording
      - add accuracy recording (macro, micro, per class) with gradient
      - add clipped_proportion for each class with gradient

--- dpdl/callbacks.py
+++ dpdl/callbacks.py
@@ -13,6 +13,7 @@ from .configurationmanager import Configuration, Hyperparameters
 
 log = logging.getLogger(__name__)
 
+
 class CallbackHandler:
     def __init__(self, callbacks: list = []):
         self.callbacks = callbacks
@@ -22,89 +23,117 @@ class CallbackHandler:
             event_handler = getattr(callback, event)
             event_handler(*args, **kwargs)
 
+
 class Callback:
     def _is_global_zero(self, trainer):
         return torch.distributed.get_rank() == 0
+
     def on_train_start(self, trainer):
         pass
+
     def on_train_end(self, trainer):
         pass
+
     def on_train_epoch_start(self, trainer, epoch):
         pass
+
     def on_train_epoch_end(self, trainer, epoch, epoch_loss):
         pass
+
     def on_train_batch_start(self, trainer, batch_idx, batch):
         pass
+
     def on_train_batch_end(self, trainer, batch_idx, batch, loss):
         pass
+
     def on_validation_epoch_start(self, trainer, epoch):
         pass
+
     def on_validation_epoch_end(self, trainer, epoch, metrics):
         pass
+
     def on_validation_batch_start(self, trainer, batch_idx, batch):
         pass
+
     def on_validation_batch_end(self, trainer, batch_idx, batch, loss):
         pass
+
     def on_test_epoch_start(self, trainer, epoch):
         pass
+
     def on_test_epoch_end(self, trainer, epoch, metrics):
         pass
+
     def on_test_batch_start(self, trainer, batch_idx, batch):
         pass
+
     def on_test_batch_end(self, trainer, batch_idx, batch, loss):
         pass
 
-    def _log_metrics(self, metrics, annotation='Metrics'):
+    def _log_metrics(self, metrics, annotation="Metrics"):
         if not metrics:
             return
 
-        log.info(annotation + ':')
+        log.info(annotation + ":")
         for key, value in metrics.items():
-            log.info(f' - {key}: {value:.4f}.')
+            if isinstance(value, torch.Tensor):
+                if value.dim() == 0:
+                    value = float(value)
+                    log.info(f" - {key}: {value:.4f}.")
+                else:
+                    value = value.tolist()
+                    log.info(f" - {key}: {value}.")
+            else:
+                log.info(f" - {key}: {value:.4f}.")
+
 
 class RecordEpochStatsCallback(Callback):
     def __init__(self, use_steps=False):
         self.use_steps = use_steps
 
         self.train_loss = torchmetrics.aggregation.MeanMetric().cuda()
-        self.evaluation_loss = torchmetrics.aggregation.MeanMetric(sync_on_compute=False).cuda()
+        self.evaluation_loss = torchmetrics.aggregation.MeanMetric(
+            sync_on_compute=False
+        ).cuda()
 
     def on_train_start(self, trainer):
         if self._is_global_zero(trainer):
             if self.use_steps:
                 batch_size = trainer.datamodule.batch_size
-                data_size = len(trainer.get_dataloader('train').dataset)
+                data_size = len(trainer.get_dataloader("train").dataset)
                 steps_per_epoch = data_size / batch_size
                 epochs = math.ceil(trainer.total_steps / steps_per_epoch)
 
-                log.info(f'!!! Starting training for approximately {epochs} epochs ({trainer.total_steps} steps).')
+                log.info(
+                    f"!!! Starting training for approximately {epochs} epochs ({trainer.total_steps} steps)."
+                )
             else:
-                log.info(f'!!! Starting training for {trainer.epochs} epochs.')
+                log.info(f"!!! Starting training for {trainer.epochs} epochs.")
 
     def on_train_end(self, trainer):
         if self._is_global_zero(trainer):
-            log.info('!!! Training finished.')
+            log.info("!!! Training finished.")
 
     def on_train_epoch_start(self, trainer, epoch):
         self.train_loss.reset()
 
         if self._is_global_zero(trainer):
-            log.info(f'--------------------------------------------------')
+            log.info(f"--------------------------------------------------")
             if not self.use_steps:
-                log.info(f'Starting training epoch {epoch+1}.')
+                log.info(f"Starting training epoch {epoch+1}.")
             else:
-                log.info(f'Starting training approximate epoch {epoch+1}.')
+                log.info(f"Starting training approximate epoch {epoch+1}.")
 
     def on_train_epoch_end(self, trainer, epoch, metrics):
         loss = self.train_loss.compute()
 
         if self._is_global_zero(trainer):
             if not self.use_steps:
-                log.info(f'Epoch {epoch+1} finished. Loss: {loss:.4f}.')
+                log.info(f"Epoch {epoch+1} finished. Loss: {loss:.4f}.")
             else:
-                log.info(f'Approximate epoch {epoch+1} finished. Loss: {loss:.4f}.')
+                log.info(f"Approximate epoch {epoch+1} finished. Loss: {loss:.4f}.")
 
-            self._log_metrics(metrics, 'Train metrics')
+            self._log_metrics(metrics, "Train metrics")
 
     def on_train_batch_end(self, trainer, batch_idx, batch, loss):
         self.train_loss.update(loss)
@@ -114,8 +143,8 @@ class RecordEpochStatsCallback(Callback):
         self.evaluation_loss.reset()
 
         if torch.distributed.get_rank() == 0:
-            log.info(f'Validation finished. Loss: {loss:.4f}.')
-            self._log_metrics(metrics, 'Validation metrics')
+            log.info(f"Validation finished. Loss: {loss:.4f}.")
+            self._log_metrics(metrics, "Validation metrics")
 
     def on_validation_batch_end(self, trainer, batch_idx, batch, loss):
         self.evaluation_loss.update(loss)
@@ -125,11 +154,12 @@ class RecordEpochStatsCallback(Callback):
         self.evaluation_loss.reset()
 
         if torch.distributed.get_rank() == 0:
-            log.info(f'Test finished. Loss: {loss:.4f}.')
-            self._log_metrics(metrics, 'Test metrics')
+            log.info(f"Test finished. Loss: {loss:.4f}.")
+            self._log_metrics(metrics, "Test metrics")
 
     def on_test_batch_end(self, trainer, batch_idx, batch, loss):
         self.evaluation_loss.update(loss)
+
 
 class RecordSNR(Callback):
     def __init__(self, log_dir: str = None):
@@ -141,7 +171,9 @@ class RecordSNR(Callback):
     def on_train_batch_end(self, trainer):
         grad_norm = trainer.optimizer._previous_grad.norm().item()
         noise_norm = trainer.optimizer._previous_noise.norm().item()
-        snr = grad_norm / noise_norm if noise_norm != 0 else float('inf')  # NB: div by zero
+        snr = (
+            grad_norm / noise_norm if noise_norm != 0 else float("inf")
+        )  # NB: div by zero
 
         self.grad_norms.append(grad_norm)
         self.noise_norms.append(noise_norm)
@@ -149,16 +181,24 @@ class RecordSNR(Callback):
 
     def on_train_end(self, trainer, *args, **kwargs):
         if torch.distributed.get_rank() == 0:
-            file_path = os.path.join(self.log_dir, 'signal-to-noise-ratio.csv')
+            file_path = os.path.join(self.log_dir, "signal-to-noise-ratio.csv")
 
-            with open(file_path, 'w', newline='') as fh:
+            with open(file_path, "w", newline="") as fh:
                 writer = csv.writer(fh)
-                writer.writerow(['Step', 'Grad_Norm', 'Noise_Norm', 'SNR'])
+                writer.writerow(["Step", "Grad_Norm", "Noise_Norm", "SNR"])
 
                 for step in range(len(self.grad_norms)):
-                    writer.writerow([step, self.grad_norms[step], self.noise_norms[step], self.snr_values[step]])
+                    writer.writerow(
+                        [
+                            step,
+                            self.grad_norms[step],
+                            self.noise_norms[step],
+                            self.snr_values[step],
+                        ]
+                    )
 
-            log.info(f'Signal-to-Noise ratio data saved at {file_path}')
+            log.info(f"Signal-to-Noise ratio data saved at {file_path}")
+
 
 class RecordGradientNormsCallback(Callback):
     def __init__(self, log_dir: str = None, max_grad_norm: float = 0.0):
@@ -193,18 +233,24 @@ class RecordGradientNormsCallback(Callback):
                 )
             else:
                 for layer_index in range(len(per_layer_per_sample_norms)):
-                    self.per_layer_per_sample_norms_current_epoch[class_label.item()][layer_index] = torch.cat(
-                            (
-                                self.per_layer_per_sample_norms_current_epoch[class_label.item()][layer_index],
-                                per_layer_per_sample_norms[layer_index][class_mask]
-                                ),
-                                dim=0
-                            )
+                    self.per_layer_per_sample_norms_current_epoch[class_label.item()][
+                        layer_index
+                    ] = torch.cat(
+                        (
+                            self.per_layer_per_sample_norms_current_epoch[
+                                class_label.item()
+                            ][layer_index],
+                            per_layer_per_sample_norms[layer_index][class_mask],
+                        ),
+                        dim=0,
+                    )
             # compute the proportion of clipped gradients for each class
             per_sample_norms = torch.norm(
                 torch.stack(per_layer_per_sample_norms[class_mask]), p=2, dim=0
             )
-            clipped_num_this_class = sum([g.gt(self.max_grad_norm) for g in per_sample_norms])
+            clipped_num_this_class = sum(
+                [g.gt(self.max_grad_norm) for g in per_sample_norms]
+            )
             total_num_this_class = sum(class_mask)
             self.clipped_current_epoch[class_label] += clipped_num_this_class
             self.total_current_epoch[class_label] += total_num_this_class
@@ -215,24 +261,32 @@ class RecordGradientNormsCallback(Callback):
         # computing the mean norm over weights and bias
         if self._is_global_zero(trainer):
             layer_mean_norms_per_class = {}
-            for class_label, layer_norms_per_class in self.per_layer_per_sample_norms_current_epoch.items():
+            for (
+                class_label,
+                layer_norms_per_class,
+            ) in self.per_layer_per_sample_norms_current_epoch.items():
                 mean_layer_norms_per_class = []
                 for layer_norms in layer_norms_per_class:
-                    mean_layer_norms_per_class.append(torch.mean(layer_norms, dim=0).item())
+                    mean_layer_norms_per_class.append(
+                        torch.mean(layer_norms, dim=0).item()
+                    )
                 layer_mean_norms_per_class[class_label] = mean_layer_norms_per_class
 
             # compute the proportion of clipped gradients for each class
             clipped_proportion = {}
             for class_label in self.clipped_current_epoch.keys():
-                clipped_proportion[class_label] = self.clipped_current_epoch[class_label] / self.total_current_epoch[class_label]
+                clipped_proportion[class_label] = (
+                    self.clipped_current_epoch[class_label]
+                    / self.total_current_epoch[class_label]
+                )
 
             # save
             self.per_layer_norms_history.append(
                 {
-                    'step': epoch,
-                    'data': layer_mean_norms_per_class,
-                    'accuracy': epoch_loss,
-                    'clipped_proportion': self.clipped_proportion_history,
+                    "step": epoch,
+                    "data": layer_mean_norms_per_class,
+                    "accuracy": epoch_loss,
+                    "clipped_proportion": self.clipped_proportion_history,
                 }
             )
 
@@ -241,66 +295,68 @@ class RecordGradientNormsCallback(Callback):
             self.clipped_current_epoch = defaultdict(int)
             self.total_current_epoch = defaultdict(int)
 
-
     def on_train_end(self, trainer, *args, **kwargs):
         if self._is_global_zero(trainer):
-            file_path = os.path.join(self.log_dir, f'gradient_norms_last.json')
-            with open(file_path, 'w') as fh:
+            file_path = os.path.join(self.log_dir, f"gradient_norms_last.json")
+            with open(file_path, "w") as fh:
                 json.dump(self.per_layer_norms_history, fh)
 
-            log.info(f'Gradient norm data saved to {file_path}')
+            log.info(f"Gradient norm data saved to {file_path}")
 
 
 class DebugProbeCallback(Callback):
     def _is_global_zero(self, trainer):
-        log.info(f'[DEBUG] Calling _is_global_zero')
+        log.info(f"[DEBUG] Calling _is_global_zero")
         return torch.distributed.get_rank() == 0
 
     def on_train_start(self, trainer):
-        log.info(f'[DEBUG] on_train_start')
+        log.info(f"[DEBUG] on_train_start")
 
     def on_train_end(self, trainer):
-        log.info(f'[DEBUG] on_train_end')
+        log.info(f"[DEBUG] on_train_end")
 
     def on_train_epoch_start(self, trainer, epoch):
-        log.info(f'[DEBUG] on_train_epoch_start')
+        log.info(f"[DEBUG] on_train_epoch_start")
 
     def on_train_epoch_end(self, trainer, epoch, epoch_loss):
-        log.info(f'[DEBUG] on_train_epoch_end')
+        log.info(f"[DEBUG] on_train_epoch_end")
 
     def on_train_batch_start(self, trainer, batch_idx, batch):
-        log.info(f'[DEBUG] on_train_batch_start')
+        log.info(f"[DEBUG] on_train_batch_start")
 
     def on_train_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f'[DEBUG] on_train_batch_end')
+        log.info(f"[DEBUG] on_train_batch_end")
 
     def on_validation_epoch_start(self, trainer, epoch):
-        log.info(f'[DEBUG] on_validation_epoch_start')
+        log.info(f"[DEBUG] on_validation_epoch_start")
 
     def on_validation_epoch_end(self, trainer, epoch, metrics):
-        log.info(f'[DEBUG] on_validation_epoch_end')
+        log.info(f"[DEBUG] on_validation_epoch_end")
 
     def on_validation_batch_start(self, trainer, batch_idx, batch):
-        log.info(f'[DEBUG] on_validation_batch_start')
+        log.info(f"[DEBUG] on_validation_batch_start")
 
     def on_validation_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f'[DEBUG] on_validation_batch_end')
+        log.info(f"[DEBUG] on_validation_batch_end")
 
     def on_test_epoch_start(self, trainer, epoch):
-        log.info(f'[DEBUG] on_test_epoch_start')
+        log.info(f"[DEBUG] on_test_epoch_start")
 
     def on_test_epoch_end(self, trainer, epoch, metrics):
-        log.info(f'[DEBUG] on_test_epoch_end')
+        log.info(f"[DEBUG] on_test_epoch_end")
 
     def on_test_batch_start(self, trainer, batch_idx, batch):
-        log.info(f'[DEBUG] on_test_batch_start')
+        log.info(f"[DEBUG] on_test_batch_start")
 
     def on_test_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f'[DEBUG] on_test_batch_end')
+        log.info(f"[DEBUG] on_test_batch_end")
+
 
 class CallbackFactory:
     @staticmethod
-    def get_callbacks(configuration: Configuration, hyperparams: Hyperparameters) -> List[Callback]:
+    def get_callbacks(
+        configuration: Configuration, hyperparams: Hyperparameters
+    ) -> List[Callback]:
         callbacks = [
             RecordEpochStatsCallback(use_steps=configuration.use_steps),
         ]
@@ -308,19 +364,22 @@ class CallbackFactory:
         if configuration.record_snr:
             log_dir = configuration.log_dir
             experiment_name = configuration.experiment_name
-            full_log_dir = pathlib.Path(f'{log_dir}/{experiment_name}')
+            full_log_dir = pathlib.Path(f"{log_dir}/{experiment_name}")
 
             callbacks.append(RecordSNR(log_dir=full_log_dir))
 
         if configuration.record_gradient_norms:
             log_dir = configuration.log_dir
-            full_log_dir = pathlib.Path(f'{log_dir}/{experiment_name}')
+            full_log_dir = pathlib.Path(f"{log_dir}/{experiment_name}")
             max_grad_norm = hyperparams.max_grad_norm
 
-            callbacks.append(RecordGradientNormsCallback(log_dir=full_log_dir, max_grad_norm=max_grad_norm))
+            callbacks.append(
+                RecordGradientNormsCallback(
+                    log_dir=full_log_dir, max_grad_norm=max_grad_norm
+                )
+            )
 
         if configuration.verbose_callback:
             callbacks.append(DebugProbeCallback())
-
 
         return callbacks
