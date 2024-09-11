802714333c66fe8b78e8d8108fcb8b6c381453dc
Rehn Aki <aki.rehn@helsinki.fi>
2024-09-11 09:33:42 +0300

    Minor changes to gradient recordsing.
    - Add function for converting Tensor to Python types.
    - Move imports to top level.
    - Make `log_dir` and `max_grad_norm` required parameters.

--- dpdl/utils.py
+++ dpdl/utils.py
@@ -14,3 +14,15 @@ def seed_everything(seed) -> None:
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+def tensor_to_python_type(data):
+    """
+    Recursively converts all tensors in a dictionary or list to Python native types.
+    """
+    if isinstance(data, torch.Tensor):
+        return data.item() if data.numel() == 1 else data.tolist()
+    elif isinstance(data, dict):
+        return {key: tensor_to_python_type(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [tensor_to_python_type(item) for item in data]
+    else:
+        return data
