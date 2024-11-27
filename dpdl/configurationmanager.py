bd5540e879c99f3d4756affbcd2a791fcb676bf3
Linzh <linzh.zhao@outlook.com>
2024-11-27 14:23:32 +0200

    update: remove the 'record_hpo_metrics' in cli as it is a light operation
      - fix the double quote to single quote
      - remove config 'record_hpo_metrics' from all place, i.e., it is enabled by default

--- dpdl/configurationmanager.py
+++ dpdl/configurationmanager.py
@@ -96,7 +96,6 @@ class Configuration(BaseModel):
     model_save_fpath: Optional[str] = None
     record_gradient_norms: Optional[bool] = False
     verbose_callback: Optional[bool] = False
-    record_hpo_metrics: Optional[bool] = False
     cache_dataset_transforms: Optional[bool] = False
     weight_perturbation_level: float = 0
 
@@ -190,7 +189,6 @@ class Configuration(BaseModel):
             ('Model save file path', self.model_save_fpath),
             ('Record gradient norms', self.record_gradient_norms),
             ('Enable the debug callback output',self.verbose_callback),
-            ('Record HPO metrics', self.record_hpo_metrics),
         ]
 
         if self.privacy:
