83d157dc8dfb8c187e39e76dd8e7a54e5cdeda1f
Aki Rehn <aki.rehn@helsinki.fi>
2025-01-10 15:09:20 +0200

    Add run scripts for experiment 21 (hyper grid).
    - In experiment 19 where we did the hyperparameter grid, we did not
      record the gradient statistics at all. Created a new experiment for
      running the same grid, but now also recording the gradient statistics.

---
+++ experiments/21-hyperparameter-grid-with-gradient-recording.md
@@ -0,0 +1,4 @@
+# Hyperparameter grid with gradient recording
+
+This is just a redo of [the hyperparameter grid experiment](./19-hyperparameter-grid.md) with gradient recording enabled. Mainpurpose of having a separate experiment is to keep the old and new data separate.
+
---
+++ experiments/21-hyperparameter-grid-with-gradient-recording/scripts/run_and_resume_patch_camelyon.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+############################################################
+### Experiment: Hyperparameter grid (2% of Patch Camelyon)
+############################################################
+
+set -euo pipefail
+
+# Base configurations
+EXPERIMENT_BASE="21-hyperparameter-grid-with-gradient-recording"
+LOG_DIR="/projappl/$PROJECT/dpdl/experiments/$EXPERIMENT_BASE/data_patch_camelyon_epsilon8"
+mkdir -p $LOG_DIR
+
+# Experiment parameters
+MODELS=("vit_base_patch16_224.augreg_in21k")
+DATASETS=(
+    "dpdl-benchmark/patch_camelyon"
+#    "dpdl-benchmark/sun397"
+#    "dpdl-benchmark/svhn_cropped_balanced"
+#    "cifar100"
+)
+
+EPOCHS=40
+LEARNING_RATES="0.0005 0.0009596915518332421 0.001842015749320193 0.003535533905932737 0.006786044041487265 0.013025018273967286 0.025"
+BATCH_SIZES="192 512 1024 2048 4096 -1"
+MAX_GRAD_NORMS="0.001 0.004135185542000139 0.01709975946676697 0.07071067811865475 0.2924017738212867 1.2091355875609793 5.0"
+EPSILONS="8"
+SEEDS="42"
+
+declare -A DATASET_LABEL_FIELDS
+DATASET_LABEL_FIELDS=(
+    ["dpdl-benchmark/patch_camelyon"]="label"
+    ["dpdl-benchmark/sun397"]="label"
+    ["dpdl-benchmark/svhn_cropped_balanced"]="label"
+    ["cifar100"]="fine_label"
+)
+
+declare -A SUBSET_SIZES
+SUBSET_SIZES=(
+    ["dpdl-benchmark/patch_camelyon"]="0.02"
+    ["dpdl-benchmark/sun397"]="0.1"
+    ["dpdl-benchmark/svhn_cropped_balanced"]="0.1"
+    ["cifar100"]="0.1"
+)
+
+# Other settings
+OVERWRITE_EXPERIMENT="--overwrite-experiment" # These are fast to run, overwrite by default
+PRIVACY="--privacy"
+USE_STEPS="--use-steps"
+NORMALIZE_CLIPPING="--normalize-clipping"
+ZERO_HEAD="--zero-head"
+PEFT="--peft film"
+RECORD_LOSS="--record-loss-by-step --record-loss-by-epoch"
+EVALUATION_MODE="--evaluation-mode"
+RECORD_GRADIENTS="--record-gradient-norms"
+
+# Function to check if a job is already in queue
+function is_job_in_queue() {
+    local experiment_name=$1
+    if squeue --me -o "%.150j" | grep -q "$experiment_name"; then
+        return 0  # Job is in queue
+    else
+        return 1  # Job is not in queue
+    fi
+}
+
+for seed in $SEEDS
+do
+    # Loop over configurations
+    for model in "${MODELS[@]}"
+    do
+        for dataset in "${DATASETS[@]}"
+        do
+            # Get the label field name for this dataset
+            label_field=${DATASET_LABEL_FIELDS[$dataset]}
+
+            # Get the configured subset size for this dataset
+            subset_size=${SUBSET_SIZES[$dataset]}
+
+            # Remove possible prefix from the dataset name
+            clean_dataset_name=${dataset#dpdl-benchmark/}
+
+            for batch_size in $BATCH_SIZES
+            do
+                for learning_rate in $LEARNING_RATES
+                do
+                    rounded_learning_rate=$(printf "%.4f" $learning_rate)
+
+                    for max_grad_norm in $MAX_GRAD_NORMS
+                    do
+                        rounded_max_grad_norm=$(printf "%.4f" $max_grad_norm)
+
+                        for epsilon in $EPSILONS
+                        do
+                            rounded_epsilon=$(printf "%.2f" $epsilon)
+
+                            EXPERIMENT_NAME="${model}_${clean_dataset_name}_Subset${subset_size}_Epsilon${rounded_epsilon}_BatchSize${batch_size}_LearningRate${rounded_learning_rate}_MaxGradNorm${rounded_max_grad_norm}_Seed${seed}"
+
+                            EXPERIMENT_DIR="$LOG_DIR/$EXPERIMENT_NAME"
+                            mkdir -p "$EXPERIMENT_DIR"
+
+                            # If `runtime` file exists, then the job has been completed
+                            if [ -f "$EXPERIMENT_DIR/runtime" ]; then
+                                echo "Experiment $EXPERIMENT_NAME has completed. Skipping submission."
+                                continue
+                            fi
+
+                            if is_job_in_queue $EXPERIMENT_NAME; then
+                                echo "Experiment $EXPERIMENT_NAME is already in the queue."
+                                continue  # Skip to the next iteration
+                            fi
+
+                            # Submit the job
+                            sbatch -J $EXPERIMENT_NAME run8-rocm.sh run.py train \
+                                --num-workers 7 \
+                                --model-name $model \
+                                --dataset-name $dataset \
+                                --subset-size $subset_size \
+                                --dataset-label-field $label_field \
+                                --batch-size $batch_size \
+                                --learning-rate $learning_rate \
+                                --max-grad-norm $max_grad_norm \
+                                --epochs $EPOCHS \
+                                --target-epsilon $epsilon \
+                                --seed $seed \
+                                --physical-batch-size 40 \
+                                --experiment-name $EXPERIMENT_NAME \
+                                --log-dir $LOG_DIR \
+                                $ZERO_HEAD \
+                                $PEFT \
+                                $PRIVACY \
+                                $USE_STEPS \
+                                $NORMALIZE_CLIPPING \
+                                $OVERWRITE_EXPERIMENT \
+                                $RECORD_LOSS \
+                                $RECORD_GRADIENTS \
+                                $EVALUATION_MODE
+
+                            SBATCH_EXIT_CODE=$?
+
+                            if [ $SBATCH_EXIT_CODE -eq 0 ]; then
+                                echo "Job $EXPERIMENT_NAME submitted successfully."
+                            else
+                                echo "Submission of $EXPERIMENT_NAME failed with exit code $SBATCH_EXIT_CODE."
+                                exit 1
+                            fi
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
