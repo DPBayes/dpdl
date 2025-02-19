57c7cf8cb6409504f23f239ae7bd9f3d8af8c387
Aki Rehn <aki.rehn@helsinki.fi>
2025-02-19 15:19:24 +0200

    Add rank information to debug callback.
    - When the debug callback was running, it was not clear which rank emits
      the log messages. Added a information about the current rank to these
      messages.

--- dpdl/callbacks/debug.py
+++ dpdl/callbacks/debug.py
@@ -7,53 +7,53 @@ log = logging.getLogger(__name__)
 
 class DebugProbeCallback(Callback):
     def _is_global_zero(self):
-        log.info(f"[DEBUG] Calling _is_global_zero")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] Calling _is_global_zero")
         super().__is_global_zero()
 
     def on_train_start(self, trainer):
-        log.info(f"[DEBUG] on_train_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_start")
 
     def on_train_end(self, trainer):
-        log.info(f"[DEBUG] on_train_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_end")
 
     def on_train_epoch_start(self, trainer, epoch):
-        log.info(f"[DEBUG] on_train_epoch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_epoch_start")
 
     def on_train_epoch_end(self, trainer, epoch, epoch_loss):
-        log.info(f"[DEBUG] on_train_epoch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_epoch_end")
 
     def on_train_batch_start(self, trainer, batch_idx, batch):
-        log.info(f"[DEBUG] on_train_batch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_batch_start")
 
     def on_train_physical_batch_start(self, trainer, batch_idx, batch):
-        log.info(f"[DEBUG] on_train_physical_batch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_physical_batch_start")
 
     def on_train_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f"[DEBUG] on_train_batch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_batch_end")
 
     def on_train_physical_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f"[DEBUG] on_train_physical_batch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_train_physical_batch_end")
 
     def on_validation_epoch_start(self, trainer, epoch):
-        log.info(f"[DEBUG] on_validation_epoch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_validation_epoch_start")
 
     def on_validation_epoch_end(self, trainer, epoch, metrics):
-        log.info(f"[DEBUG] on_validation_epoch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_validation_epoch_end")
 
     def on_validation_batch_start(self, trainer, batch_idx, batch):
-        log.info(f"[DEBUG] on_validation_batch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_validation_batch_start")
 
     def on_validation_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f"[DEBUG] on_validation_batch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_validation_batch_end")
 
     def on_test_epoch_start(self, trainer, epoch):
-        log.info(f"[DEBUG] on_test_epoch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_test_epoch_start")
 
     def on_test_epoch_end(self, trainer, epoch, metrics):
-        log.info(f"[DEBUG] on_test_epoch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_test_epoch_end")
 
     def on_test_batch_start(self, trainer, batch_idx, batch):
-        log.info(f"[DEBUG] on_test_batch_start")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_test_batch_start")
 
     def on_test_batch_end(self, trainer, batch_idx, batch, loss):
-        log.info(f"[DEBUG] on_test_batch_end")
+        log.info(f"[DEBUG - RANK {torch.distributed.get_rank()}] on_test_batch_end")
