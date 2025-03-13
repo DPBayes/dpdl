148031c6a3359290ba3825f8e2b39008b222cb67
Aki Rehn <aki.rehn@helsinki.fi>
2025-03-13 14:25:54 +0200

    Update experiment 28 script.
    - Removed class imbalancing stuff. Make CB grid tighter.
    - Forgot to commit this earlier...

--- experiments/28-difficult-examples-prediction-errors/scripts/run_and_resume.sh
+++ experiments/28-difficult-examples-prediction-errors/scripts/run_and_resume.sh
@@ -21,7 +21,7 @@ DATASETS=(
 )
 EPOCHS=40
 EPSILONS="1 2 4 8"
-MAX_GRAD_NORMS="0.0001 0.01 1 10"
+MAX_GRAD_NORMS="0.00001 0.0001 0.001 0.01 0.1 1 10"
 IMBALANCE_FACTORS="0.5 0.1"
 IMBALANCE_CLASS="6"
 OPTUNA_CONFIG="conf/optuna_hypers-difficult-examples-prediction-errors.conf"
@@ -85,98 +85,93 @@ do
             do
                 rounded_epsilon=$(printf "%.4f" $epsilon)
 
-                for imbalance_factor in $IMBALANCE_FACTORS
-                do
-                    EXPERIMENT_NAME="${model}_${clean_dataset_name}_Subset${subset_size}_Epsilon${rounded_epsilon}_MaxGradNorm${max_grad_norm}_ImbalanceClass${IMBALANCE_CLASS}_ImbalanceFactor${imbalance_factor}"
-                    EXPERIMENT_DIR="$LOG_DIR/$EXPERIMENT_NAME"
-                    mkdir -p "$EXPERIMENT_DIR"
+                EXPERIMENT_NAME="${model}_${clean_dataset_name}_Subset${subset_size}_Epsilon${rounded_epsilon}_MaxGradNorm${max_grad_norm}"
+                EXPERIMENT_DIR="$LOG_DIR/$EXPERIMENT_NAME"
+                mkdir -p "$EXPERIMENT_DIR"
 
-                    MODEL_SAVE_PATH="$EXPERIMENT_DIR/final_model.pt"
+                MODEL_SAVE_PATH="$EXPERIMENT_DIR/final_model.pt"
 
-                    # If `runtime` file exists, then the job has been completed
-                    if [ -f "$EXPERIMENT_DIR/runtime" ]; then
-                        echo "Experiment $EXPERIMENT_NAME has completed. Skipping submission."
-                        continue
-                    fi
+                # If `runtime` file exists, then the job has been completed
+                #if [ -f "$EXPERIMENT_DIR/runtime" ]; then
+                #    echo "Experiment $EXPERIMENT_NAME has completed. Skipping submission."
+                #    continue
+                #fi
 
-                    # Determine if we should attempt to resume based on job submission status
-                    if is_job_submitted $EXPERIMENT_NAME; then
-                        N_TRIALS=$(python bin/get_n_trials.py --optuna-journal "$OPTUNA_JOURNAL" --study-name "$EXPERIMENT_NAME")
+                # Determine if we should attempt to resume based on job submission status
+                if is_job_submitted $EXPERIMENT_NAME; then
+                    N_TRIALS=$(python bin/get_n_trials.py --optuna-journal "$OPTUNA_JOURNAL" --study-name "$EXPERIMENT_NAME")
 
-                        if [ "$N_TRIALS" -eq -1 ]; then
-                            # If N_TRIALS is -1, then the job has never been started
-                            REMAINING_TRIALS=$DEFAULT_N_TRIALS
-                            OPTUNA_RESUME="--no-optuna-resume"
-                            OVERWRITE_EXPERIMENT="--overwrite-experiment"
-
-                            echo "New job: $EXPERIMENT_NAME"
-                        else
-                            # Otherwise, we want to resume it with the remaining trials
-                            REMAINING_TRIALS=$((DEFAULT_N_TRIALS - N_TRIALS))
-                            OPTUNA_RESUME="--optuna-resume"
-                            OVERWRITE_EXPERIMENT="--no-overwrite-experiment"
-
-                            echo "Resuming job: $EXPERIMENT_NAME with $REMAINING_TRIALS trials"
-                        fi
-                    else
-                        # Job not submitted, treat as new job
-                        N_TRIALS=-1
+                    if [ "$N_TRIALS" -eq -1 ]; then
+                        # If N_TRIALS is -1, then the job has never been started
                         REMAINING_TRIALS=$DEFAULT_N_TRIALS
                         OPTUNA_RESUME="--no-optuna-resume"
                         OVERWRITE_EXPERIMENT="--overwrite-experiment"
+
                         echo "New job: $EXPERIMENT_NAME"
-                    fi
-
-                    if is_job_in_queue $EXPERIMENT_NAME; then
-                        echo "Experiment $EXPERIMENT_NAME is already in the queue."
-                        continue  # Skip to the next iteration
-                    fi
-
-                    # Submit the job
-                    sbatch -J $EXPERIMENT_NAME run8.sh run.py optimize \
-                        --num-workers 7 \
-                        --model-name $model \
-                        --dataset-name $dataset \
-                        --subset-size $subset_size \
-                        --dataset-label-field $label_field \
-                        --max-grad-norm $max_grad_norm \
-                        --target-hypers learning_rate \
-                        --target-hypers batch_size \
-                        --epochs $EPOCHS \
-                        --target-epsilon $epsilon \
-                        --n-trials $REMAINING_TRIALS \
-                        --seed $SEED \
-                        --physical-batch-size 40 \
-                        --optuna-config $OPTUNA_CONFIG \
-                        --optuna-target-metric MulticlassAccuracy \
-                        --optuna-direction maximize \
-                        --experiment-name $EXPERIMENT_NAME \
-                        --log-dir $LOG_DIR \
-                        $ZERO_HEAD \
-                        $PEFT \
-                        $PRIVACY \
-                        $USE_STEPS \
-                        $NORMALIZE_CLIPPING \
-                        $OVERWRITE_EXPERIMENT \
-                        $OPTUNA_RESUME \
-                        --optuna-journal $OPTUNA_JOURNAL \
-                        $RECORD \
-                        "$SAVE_MODEL $MODEL_SAVE_PATH" \
-                        --imbalance-factor $imbalance_factor \
-                        --fairness-imbalance-class $IMBALANCE_CLASS
-
-                    SBATCH_EXIT_CODE=$?
-
-                    if [ $SBATCH_EXIT_CODE -eq 0 ]; then
-                        echo "Job $EXPERIMENT_NAME submitted successfully."
-                        if [ "$N_TRIALS" -eq -1 ]; then
-                            echo $EXPERIMENT_NAME >> $JOB_STATUS_LOG  # Log the submission for new experiments only
-                        fi
                     else
-                        echo "Submission of $EXPERIMENT_NAME failed with exit code $SBATCH_EXIT_CODE."
-                        exit 1
+                        # Otherwise, we want to resume it with the remaining trials
+                        REMAINING_TRIALS=$((DEFAULT_N_TRIALS - N_TRIALS))
+                        OPTUNA_RESUME="--optuna-resume"
+                        OVERWRITE_EXPERIMENT="--no-overwrite-experiment"
+
+                        echo "Resuming job: $EXPERIMENT_NAME with $REMAINING_TRIALS trials"
                     fi
-                done
+                else
+                    # Job not submitted, treat as new job
+                    N_TRIALS=-1
+                    REMAINING_TRIALS=$DEFAULT_N_TRIALS
+                    OPTUNA_RESUME="--no-optuna-resume"
+                    OVERWRITE_EXPERIMENT="--overwrite-experiment"
+                    echo "New job: $EXPERIMENT_NAME"
+                fi
+
+                if is_job_in_queue $EXPERIMENT_NAME; then
+                    echo "Experiment $EXPERIMENT_NAME is already in the queue."
+                    continue  # Skip to the next iteration
+                fi
+
+                # Submit the job
+                sbatch -J $EXPERIMENT_NAME run8.sh run.py optimize \
+                    --num-workers 7 \
+                    --model-name $model \
+                    --dataset-name $dataset \
+                    --subset-size $subset_size \
+                    --dataset-label-field $label_field \
+                    --max-grad-norm $max_grad_norm \
+                    --target-hypers learning_rate \
+                    --target-hypers batch_size \
+                    --epochs $EPOCHS \
+                    --target-epsilon $epsilon \
+                    --n-trials $REMAINING_TRIALS \
+                    --seed $SEED \
+                    --physical-batch-size 40 \
+                    --optuna-config $OPTUNA_CONFIG \
+                    --optuna-target-metric MulticlassAccuracy \
+                    --optuna-direction maximize \
+                    --experiment-name $EXPERIMENT_NAME \
+                    --log-dir $LOG_DIR \
+                    $ZERO_HEAD \
+                    $PEFT \
+                    $PRIVACY \
+                    $USE_STEPS \
+                    $NORMALIZE_CLIPPING \
+                    $OVERWRITE_EXPERIMENT \
+                    $OPTUNA_RESUME \
+                    --optuna-journal $OPTUNA_JOURNAL \
+                    $RECORD \
+                    "$SAVE_MODEL $MODEL_SAVE_PATH"
+
+                SBATCH_EXIT_CODE=$?
+
+                if [ $SBATCH_EXIT_CODE -eq 0 ]; then
+                    echo "Job $EXPERIMENT_NAME submitted successfully."
+                    if [ "$N_TRIALS" -eq -1 ]; then
+                        echo $EXPERIMENT_NAME >> $JOB_STATUS_LOG  # Log the submission for new experiments only
+                    fi
+                else
+                    echo "Submission of $EXPERIMENT_NAME failed with exit code $SBATCH_EXIT_CODE."
+                    exit 1
+                fi
             done
         done
     done
