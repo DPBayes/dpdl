ebada94b2d6f3269fd0146b40044ec2946b49461
Aki Rehn <aki.rehn@helsinki.fi>
2025-07-23 09:14:55 +0300

    Fix warnings cause by entry point `run.py.
    - For a while we've had warnings when the processes exits:
    ```
    [rank2]:[W722 13:41:56.134999509 ProcessGroupNCCL.cpp:1479] Warning: WARNING: destroy_process_group() was not called before program exit, which can leak resources. For more info, please see https://pytorch.org/docs/stable/distributed.html#shutdown (function operator())
    ```
    - Now it was time to fix these, since in some cases the Slurm jobs
      seemed to be stuck in `COMPLETING` state.
    - Fixed to cleanly wait for `typer.run` to finish and then setting a
      barrier so that all the ranks would complete at the same time.
    - While at it, fixed some other warnings, such as:
    ```
    UserWarning: No device id is provided via `init_process_group` or `barrier `. Using the current device set by the user.
    ```
      by excplicitly telling `init_process_group` that we are using GPU 0.
      As recommended by AMD to simplify things, our run scripts only sets one
      visible device (the correct GPU) for each rank.

--- run.py
+++ run.py
@@ -1,68 +1,16 @@
 import datetime
 import os
 import sys
+
+import multiprocess
 import torch
 import typer
-import multiprocess
 
 from dpdl.cli import cli
 from dpdl.logger_config import configure_logger
 
 
-def main():
-    log = configure_logger()
-
-    world_size = os.getenv('WORLD_SIZE')
-    rank = os.getenv('RANK')
-    local_rank = os.getenv('LOCAL_RANK')
-
-    if world_size is None or local_rank is None or rank is None:
-        log.error(
-            "Script not correctly started: Environment variables 'WORLD_SIZE', 'RANK', and 'LOCAL_RANK' missing."
-        )
-        sys.exit(1)
-
-    world_size = int(world_size)
-    local_rank = int(local_rank)
-    rank = int(rank)
-
-    log.info(
-        f'Rank {rank} initializing - our world size is {world_size} and local rank is {local_rank}.'
-    )
-
-    # Initialize the process group
-    torch.distributed.init_process_group(
-        backend='nccl',
-        world_size=world_size,
-        rank=rank,
-        timeout=datetime.timedelta(
-            minutes=60
-        ),  # Transformations can be slow, increase timeout
-    )
-
-    log.info(f'Rank {rank} initialized.')
-
-    if torch.distributed.get_rank() == 0:
-        log.info('All ranks initialized.')
-
-    typer.run(cli)
-
-    # Make sure all processes are in sync before destroying process group
-    torch.distributed.barrier()
-
-    torch.distributed.destroy_process_group()
-
-    log.info('And we are done!')
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        sys.argv.append('--help')
-
-        typer.run(cli)
-
-    torch.set_float32_matmul_precision('high')
-
+def setup_torch():
     # Enable TensorFloat-32 for performance
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
@@ -77,7 +25,71 @@ if __name__ == '__main__':
     # Set to spawn so HF datasets map work with distributed
     multiprocess.set_start_method('spawn', force=True)
 
-    if '--help' in sys.argv or '-h' in sys.argv:
+
+def main():
+    log = configure_logger()
+    setup_torch()
+
+    world_size = os.getenv('WORLD_SIZE')
+    rank = os.getenv('RANK')
+    local_rank = os.getenv('LOCAL_RANK')
+
+    if world_size is None or local_rank is None or rank is None:
+        log.error(
+            "Script not correctly started: Environment variables 'WORLD_SIZE', 'RANK', and 'LOCAL_RANK' missing."
+            "Defaulting to single GPU configuration."
+        )
+        world_size = '1'
+        rank = '0'
+        local_rank = '0'
+
+    world_size = int(world_size)
+    local_rank = int(local_rank)
+    rank = int(rank)
+
+    log.info(
+        f'Rank {rank} initializing - our world size is {world_size} and local rank is {local_rank}.'
+    )
+
+    # We only have one visible device exposed by `run_wrapper.sh` as recommended by AMD
+    torch.cuda.set_device(0)
+
+    # Initialize the process group
+    torch.distributed.init_process_group(
+        backend='nccl',
+        world_size=world_size,
+        rank=rank,
+        device_id=torch.device('cuda', 0),  # Only one visible device
+    )
+
+    log.info(f'Rank {rank} initialized.')
+
+    if torch.distributed.get_rank() == 0:
+        log.info('All ranks initialized.')
+
+    exit_code = 0
+    try:
         typer.run(cli)
+    except SystemExit as e:
+        exit_code = int(e.code) if e.code is not None else 0
+    finally:
+
+        try:
+            # Make sure all processes are in sync before destroying process group
+            torch.distributed.barrier()
+        except Exception:
+            pass  # If ranks are uneven, don't block shutdown forever
+
+        torch.distributed.destroy_process_group()
+
+        log.info(f'Rank {rank} done!')
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) == 1:
+        typer.run(cli)   # print help and exit
+        sys.exit(0)
 
     main()
