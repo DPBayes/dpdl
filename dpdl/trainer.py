abd3fbef55b8498375df29868418c455ff04c797
sebasrb09 <sebasrb09@gmail.com>
2025-10-01 14:54:58 +0300

    check callbacks

--- dpdl/trainer.py
+++ dpdl/trainer.py
@@ -145,9 +145,9 @@ class Trainer:
         for batch_idx, batch in enumerate(self.datamodule.get_dataloader('train')):
             print(self.optimizer)
             print(self.model)
-            self.callback_handler.call('on_train_batch_start', self, batch_idx, batch)
+            #self.callback_handler.call('on_train_batch_start', self, batch_idx, batch)
             logical_batch_loss = self.fit_one_batch(batch_idx, batch)
-            self.callback_handler.call('on_train_batch_end', self, batch_idx, batch, logical_batch_loss)
+            #self.callback_handler.call('on_train_batch_end', self, batch_idx, batch, logical_batch_loss)
             print('---------------------------------- end batch ------------------------')
 
         # compute the epoch metrics
@@ -181,6 +181,8 @@ class Trainer:
         # process the sub batches one at a time
         N = len(X_split)
 
+        print('Number of physical batches', N)
+
         #print('model at ',batch_idx,self.model)
 
         for i in range(N):
@@ -192,7 +194,7 @@ class Trainer:
             #print(type(y_splitted))
             physical_batch = (X_splitted, y_splitted)
 
-            self.callback_handler.call('on_train_physical_batch_start', self, i, physical_batch)
+            #self.callback_handler.call('on_train_physical_batch_start', self, i, physical_batch)
 
             logits = self.model(X_splitted)
             loss = self._unwrap_model().criterion(logits, y_splitted) / N # NB: normalize loss
@@ -210,7 +212,7 @@ class Trainer:
             self._unwrap_model().train_metrics.update(preds, y_split[i])
 
             # notify the callbacks of a physical batch end
-            self.callback_handler.call('on_train_physical_batch_end', self, i, physical_batch, loss.item())
+            #self.callback_handler.call('on_train_physical_batch_end', self, i, physical_batch, loss.item())
 
         # after accumulating the gradients for all the sub batches we can finally update weights.
         print('before step')
