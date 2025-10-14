53f169213d5e3f49154c1355a620b84aa22849d9
He <yuanhe@LM5-500-33716.local>
2025-10-14 13:38:18 +0300

    check dtype before fit_one_batch

--- dpdl/trainer.py
+++ dpdl/trainer.py
@@ -572,12 +572,6 @@ class DifferentiallyPrivateTrainer(Trainer):
                 # notify the callbacks of a physical batch start
                 self.callback_handler.call('on_train_physical_batch_start', self, batch_idx, batch)
 
-                # check shapes and dtypes for every key in the batch
-                print("[DEBUG] check shapes and dtypes for every key in the batch")
-                for k, v in batch.items():
-                    print(f"key: {k}, dtype: {v.dtype}, shape: {v.shape}")
-
-
                 # let's fit this physical batch
                 batch_loss = self.fit_one_batch(batch_idx, batch)
 
@@ -710,6 +704,11 @@ class DifferentiallyPrivateTrainer(Trainer):
                 self.callback_handler.call(
                     'on_train_physical_batch_start', self, phys_idx, batch
                 )
+
+                # check shapes and dtypes for every key in the batch
+                print("[DEBUG] check shapes and dtypes for every key in the batch")
+                for k, v in batch.items():
+                    print(f"key: {k}, dtype: {v.dtype}, shape: {v.shape}")
 
                 loss = self.fit_one_batch(phys_idx, batch)
 
