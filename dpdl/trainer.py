17cd499156b4fc6a03c046320739793488472250
sebasrb09 <sebasrb09@gmail.com>
2025-10-17 16:55:34 +0300

    small bug with setting the metrics again

--- dpdl/trainer.py
+++ dpdl/trainer.py
@@ -758,6 +758,7 @@ class TrainerFactory:
             model, transforms = ModelFactory.get_model(configuration, hyperparams, num_classes, loss_fn, None)        
             num_classes = model.config.vocab_size
             metrics = MetricsFactory.get_metrics(configuration, num_classes)
+            model.set_metrics(metrics)
         else:
             metrics = MetricsFactory.get_metrics(configuration, num_classes)
             model, transforms = ModelFactory.get_model(configuration, hyperparams, num_classes, loss_fn, metrics)        
